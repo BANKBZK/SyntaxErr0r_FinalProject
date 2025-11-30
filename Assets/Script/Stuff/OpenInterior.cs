53622c7b2c3b97461f39ccabb613047c191eefbb
BANKBZK <bankzazakung@gmail.com>
2025-11-30 22:24:45 +0700

    Scene1Update ImplementOpenInterior
    OpenInterior.cs

---
+++ Assets/Script/Stuff/OpenInterior.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor.Analytics;
+using UnityEngine;
+
+public class OpenInterior : MonoBehaviour
+{
+    public GameObject roof;
+    Collider _col;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+        if (!other.TryGetComponent<Player>(out var player)) return;
+        roof.SetActive(false);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+        if (!other.TryGetComponent<Player>(out var player)) return;
+        roof.SetActive(true);
+    }
+}
